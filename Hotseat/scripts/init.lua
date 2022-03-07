70471e65db9eeebc71e631faf021f32b3ce4eb3c
Lemonymous <10490534+Lemonymous@users.noreply.github.com>
2022-03-07 18:31:16 +0100

    v0.8.1-beta

--- Hotseat/scripts/init.lua
+++ Hotseat/scripts/init.lua
@@ -2,7 +2,7 @@
 local mod = {
 	id = "lmn_hotseat",
 	name = "Hotseat",
-	version = "0.8.0-beta",
+	version = "0.8.1-beta",
 	modApiVersion = "2.5.4",
 	icon = "img/mod_icon.png",
 	-- initialize mods that adds enemies first.
